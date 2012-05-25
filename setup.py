a897c353d6afc9e3f7794b5e4471551edd0954f4
Dexter Tad-y <dtady@cpan.org>
2012-05-25 09:11:38 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+execfile('src/cuisine_sweet/version.py')
+setup(
+    name = "cuisine_sweet",
+    version = __version__,
+    # pypi stuff
+    author = "Dexter Tad-y",
+    author_email = "dtady@cpan.org",
+    description = "Sugar-coated declarative recipes built on top of Fabric and Cuisine",
+    license = "Revised BSD License",
+    keywords = [ "fabric", "cuisine", "deployment" ],
+    url = "http://github.com/dexterbt1/cuisine_sweet",
+
+    packages = find_packages('src/'),
+    package_dir = {
+        '': 'src',
+    },
+    scripts = [],
+
+    install_requires = [
+        'Fabric',
+        'PyYAML',
+        'cuisine==0.2.6',
+        'distribute==0.6.19',
+        'pexpect',
+    ],
+
+    # could also include long_description, download_url, classifiers, etc.
+    download_url = 'https://github.com/dexterbt1/cuisine_sweet/tarball/%s' % __version__,
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+    ],
+)
---
+++ src/cuisine_sweet/version.py
@@ -0,0 +1,2 @@
+VERSION = (0,1,0)
+__version__ = '.'.join([ str(i) for i in VERSION ])
