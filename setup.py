71dda6d456288dd8d580aae7a981a3b6edb4dc1a
Dexter Tad-y <dtady@cpan.org>
2013-03-14 14:20:57 +0800

    bumped cuisine version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires = [
         'Fabric',
         'PyYAML',
-        'cuisine==0.2.6',
+        'cuisine==0.3.2',
         'distribute',
         'docutils',
         'decorator',
